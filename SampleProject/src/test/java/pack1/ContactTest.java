77803fd8e2e3804baa666f35ebdb5edfff37528f
windo <windo@DESKTOP-7QU5CGR>
2024-05-17 12:11:47 +0530

    first push

---
+++ SampleProject/src/test/java/pack1/ContactTest.java
@@ -0,0 +1,27 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontactTest()
+	{
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("chrme");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+
+
+	}
+	@Test
+	public void modifyContactTest()
+	{
+		System.out.println("execute crmodifyContactTest");
+	}
+
+}
---
+++ SampleProject/src/test/java/pak2/OrgTest.java
@@ -0,0 +1,17 @@
+package pak2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("execute create org test");
+	}
+	@Test
+	public void modifyOrgTest()
+	{
+		System.out.println("execute modifyOrgTest");
+	}
+
+}
